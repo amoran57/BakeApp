2e829ffb196124ab8e83e0975bf90f342ad6d4ec
Alex Moran <alexander.moran99@gmail.com>
2020-06-08 16:17:38 -0400

    Fix buttons at bottom, outside of ScrollView

--- BakeApp/Views/RecipeDetail.swift
+++ BakeApp/Views/RecipeDetail.swift
@@ -19,8 +19,10 @@ struct RecipeDetail: View {
     var primaryViewIsTile:Bool = defaults.bool(forKey: K.Defaults.primaryViewIsTile)
     
     var body: some View {
-        ScrollView {
+        
             VStack {
+                if primaryViewIsTile {
+                ScrollView {
                 //tease preview
                 CircleImage(image: recipe.image)
                 
@@ -55,12 +57,11 @@ struct RecipeDetail: View {
                 Spacer()
                     .frame(height: 20.0)
                 
-                if primaryViewIsTile {
                     ModelPages(recipe.instructions, currentPage: $index, currentTintColor: K.UITextColor, tintColor: K.UIFrameColor) { index, _  in
                         StepByStep(index: index, recipe: self.recipe)
                     }.frame(minHeight: 500, maxHeight: .infinity)
                         .padding(.top, -50)
-                    
+                }
                     HStack {
                         
                         Button("Show ingredients") {
@@ -88,32 +89,63 @@ struct RecipeDetail: View {
                         }.padding(.bottom)
                     }
                    
-                    
-                } else {
+        } else {
+            ScrollView {
+            //tease preview
+            CircleImage(image: recipe.image)
+            
+            //name of recipe
+            Text(recipe.name)
+                .foregroundColor(K.textColor)
+                .font(.system(size: 32))
+                .fontWeight(.bold)
+                .frame(width: 350)
+            .multilineTextAlignment(.center)
+            
+            //total time
+            Text("Total time: \(recipe.totTimeText)")
+                .foregroundColor(K.textColor)
+                .font(.system(size: 20))
+            
+            //prep and bake times
+            HStack {
+                Text("Prep time: \(recipe.prepTimeText)")
+                    .foregroundColor(K.textColor)
+                    .multilineTextAlignment(.trailing)
+                Text("|")
+                    .foregroundColor(K.textColor)
+                    .multilineTextAlignment(.center)
+                Text("Bake time: \(recipe.bakeTimeText)")
+                    .foregroundColor(K.textColor)
+                    .multilineTextAlignment(.leading)
+            }.font(.system(size: 12))
+                .frame(width: 400)
+                .fixedSize(horizontal: true, vertical: true)
+            
+            Spacer()
+                .frame(height: 20.0)
+              
                     //ingredients
                     IngList(ingList: self.recipe.ingredients)
-                    
+
                     Spacer()
                         .frame(height: 20.0)
-                    
+
                     //instructions
                     InsList(insList: self.recipe.instructions)
-                    
+                    }
                     VStack {
                         Button(action: {
                             self.showingSheet.toggle()
                         }) {
-                            HStack {
                            Text("Show step-by-step detail")
-                                Image(systemName: "arrow.right")
-                            }
                         }
                         .sheet(isPresented: $showingSheet) {
                             ScrollInsPopup(recipe: self.recipe)
                         }.padding(.bottom)
                     }.frame(width: 375, alignment: .trailing)
                         .padding(.trailing)
-                }
+                
             }
         }.navigationBarTitle("\(recipe.name)", displayMode: .inline)
             .navigationBarItems(trailing: NavigationLink(destination: Settings()) {
